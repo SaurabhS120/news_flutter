25933107a1eef219bbc33c5f63f52ae9b6e72f6a
SaurabhS120 <saurabh.sonar120@gmail.com>
2024-03-10 22:38:59 +0530

    Documentation for HomePageViewModel

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -52,14 +52,28 @@ class HomePageView extends StatelessWidget {
    );
   }
 }
+
+/// View model will handle communication between UI and data layer
+/// Responsibilities:
+/// Fetching data from data layer and notifying its response to UI
+/// To hold UI state, required controllers
+/// Calculations like validation, data filtration, sorting
 class HomePageViewModel{
   late NewsRepo newsRepo = NewsRepoDummyImpl();
   late GetNewsUseCase getNewsUseCase = GetNewsUseCase(newsRepo);
   HomePageViewModel(){
     getNewsList();
   }
+
+  /// Response subject will be private so data can only added so we can restrict adding data from this page only
   PublishSubject<Either<List<NewsModel>,BaseError>> _news_list_subject = PublishSubject<Either<List<NewsModel>,BaseError>>();
+
+  /// This getter will generate stream from private response subject
+  /// This stream will be used in page in order to make UI changes as per response
   Stream<Either<List<NewsModel>,BaseError>> get news_list_stream => _news_list_subject.stream;
+
+  /// This function will be called from UI to fetch data from data layer
+  /// After fetching data it will add data into stream which UI will listen and update UI
   void getNewsList()async{
     _news_list_subject.add(await newsRepo.getNews());
   }
