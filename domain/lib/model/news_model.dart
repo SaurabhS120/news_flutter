66e1f68a95e2d909e421c627ea0f0ad454382cec
SaurabhS120 <saurabh.sonar120@gmail.com>
2024-03-10 23:29:46 +0530

    Documentation for Model

--- domain/lib/model/news_model.dart
+++ domain/lib/model/news_model.dart
@@ -1,3 +1,7 @@
+/// This is the model class from domain layer which will contain only plain data
+/// We will use transformation methods to convert these models to data layer
+/// entities and vice versa
+/// This models will be used in presentation layer
 class NewsModel{
   final String title;
 
